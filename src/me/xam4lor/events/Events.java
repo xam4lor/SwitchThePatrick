94abecd3de7156465ca73c4db53007d53b104e4d
xam4lor <xam4lor@gmail.com>
2015-12-17 19:07:42 +0100

    Ajout des events et de la configuration du fichier config.yml .  Ajout du fichier plugin.yml

--- src/me/xam4lor/events/Events.java
+++ src/me/xam4lor/events/Events.java
@@ -1,6 +1,31 @@
 package me.xam4lor.events;
 
+import java.util.Collection;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.SkullType;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent.RegainReason;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
+import org.bukkit.event.weather.WeatherChangeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import me.xam4lor.main.MainClass;
 
@@ -9,5 +34,125 @@ public class Events implements Listener {
 	
 	public Events(MainClass m) {
 		this.m = m;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onPlayerDeath(final PlayerDeathEvent ev) {
+		Location l = ev.getEntity().getLocation();
+		Collection<? extends Player> ps = Bukkit.getServer().getOnlinePlayers();
+		
+		for (Player pp : ps) {
+			pp.playSound(pp.getLocation(), Sound.WITHER_SPAWN, 1F, 1F);
+		}
+		
+		this.m.setDeadPlayers(ev.getEntity().getName());
+		
+		Bukkit.getScheduler().runTaskLater(this.m, new BukkitRunnable() {
+			@Override
+			public void run() {
+				m.setLife((Player) ev.getEntity(), 0);
+			}
+		}, 1L);
+		
+		if (this.m.getConfig().getBoolean("kick-on-death.kick", true)) {
+			Bukkit.getScheduler().runTaskLater(this.m, new BukkitRunnable() {
+				@Override
+				public void run() {
+					ev.getEntity().kickPlayer("gg");
+				}
+			}, 20L * this.m.getConfig().getInt("kick-on-death.time", 30));
+		}
+		
+		try { 
+			ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
+			SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+			skullMeta.setOwner(((Player)ev.getEntity()).getName());
+			skullMeta.setDisplayName(ChatColor.BOLD + ((Player) ev.getEntity()).getName());
+			skull.setItemMeta(skullMeta);
+			l.getWorld().dropItem(l, skull);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerLogin(PlayerLoginEvent ev) {
+		if (this.m.isPlayerDead(ev.getPlayer().getName()) && !this.m.getConfig().getBoolean("allow-reconnect", true)) {
+			ev.setResult(Result.KICK_OTHER);
+			ev.setKickMessage("Vous êtes mort !");
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(final PlayerJoinEvent ev) {
+		if (!this.m.isGameRunning()) {
+			ev.getPlayer().setGameMode(GameMode.CREATIVE);
+			Location l = ev.getPlayer().getWorld().getSpawnLocation();
+			ev.getPlayer().teleport(l.add(0,1,0));
+		}
+	}
+	
+	@EventHandler
+	public void onBlockBreakEvent(final BlockBreakEvent ev) {
+		if (!this.m.isGameRunning()) {
+			ev.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onBlockPlaceEvent(final BlockPlaceEvent ev) {
+		if (!this.m.isGameRunning()) {
+			ev.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent ev) {
+		Location l = ev.getTo();
+		Integer mapSize = m.getConfig().getInt("map.size");
+		Integer halfMapSize = (int) Math.floor(mapSize/2);
+		Integer x = l.getBlockX();
+		Integer z = l.getBlockZ();
+		
+		Location spawn = ev.getPlayer().getWorld().getSpawnLocation();
+		Integer limitXInf = spawn.add(-halfMapSize, 0, 0).getBlockX();
+		
+		spawn = ev.getPlayer().getWorld().getSpawnLocation();
+		Integer limitXSup = spawn.add(halfMapSize, 0, 0).getBlockX();
+		
+		spawn = ev.getPlayer().getWorld().getSpawnLocation();
+		Integer limitZInf = spawn.add(0, 0, -halfMapSize).getBlockZ();
+		
+		spawn = ev.getPlayer().getWorld().getSpawnLocation();
+		Integer limitZSup = spawn.add(0, 0, halfMapSize).getBlockZ();
+		
+		if (x < limitXInf || x > limitXSup || z < limitZInf || z > limitZSup) {
+			ev.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onEntityDamage(final EntityDamageEvent ev) {
+		if (ev.getEntity() instanceof Player) {
+			if (!this.m.isGameRunning()) {
+				ev.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onEntityRegainHealth(final EntityRegainHealthEvent ev) {
+		if (ev.getRegainReason() == RegainReason.SATIATED && !m.getConfig().getBoolean("naturalRegen")) {
+			ev.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onWeatherChange(WeatherChangeEvent ev) {
+		if (!m.getConfig().getBoolean("weather")) {
+			ev.setCancelled(true);
+		}
 	}
 }
--- src/me/xam4lor/main/MainClass.java
+++ src/me/xam4lor/main/MainClass.java
@@ -1,13 +1,19 @@
 package me.xam4lor.main;
 
+import java.util.HashSet;
 import java.util.logging.Logger;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
 
 import me.xam4lor.events.Events;
 
 public class MainClass  extends JavaPlugin {
-	public Logger log = Logger.getLogger("Minecraft");
+	private Logger log = Logger.getLogger("Minecraft");
+	private boolean gameRunning = false;
+	private HashSet<String> deadPlayers = new HashSet<String>();
+	private Scoreboard sb = null;
 	
 	@Override
 	public void onEnable() {
@@ -19,8 +25,34 @@ public class MainClass  extends JavaPlugin {
 	public void onDisable() {
 		this.log.info(this.getPluginName() + "Plugin stopped");
 	}
-
+	
 	private String getPluginName() {
 		return "Switch the Patrick";
+	}
+	
+	public boolean isGameRunning() {
+		return gameRunning;
+	}
+	
+	public void setGameRunning(boolean gameRunning) {
+		this.gameRunning = gameRunning;
+	}
+
+	public HashSet<String> getDeadPlayers() {
+		return deadPlayers;
+	}
+	
+	public boolean isPlayerDead(String name) {
+		return deadPlayers.contains(name);
+	}
+
+	public void setDeadPlayers(String deadPlayers) {
+		this.deadPlayers.add(deadPlayers);
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void setLife(Player entity, int i) {
+		entity.setScoreboard(sb);
+		sb.getObjective("Vie").getScore(entity).setScore(i);
 	}
 }
